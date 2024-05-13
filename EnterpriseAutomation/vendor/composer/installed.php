6f983ce25b753d68fecdf2368eca92e5102089c3
rafzarf <xxrafzaxx@gmail.com>
2024-05-13 14:42:28 +0700

    initial commit

--- EnterpriseAutomation/vendor/composer/installed.php
+++ EnterpriseAutomation/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeigniter4/appstarter',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '374d91aad4d224dd705e9fbe9ee1b7bdfac68647',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codeigniter4/appstarter' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '374d91aad4d224dd705e9fbe9ee1b7bdfac68647',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
